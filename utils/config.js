0e622de5e32b2e55526e458cddfce557cb6f3cdc
GracieArt <daineesvang@gmail.com>
2021-08-02 20:13:11 -0500

    added environment variables

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
 var config = require("../config.json")
-config.token = process.env.TOKEN
-
+config.token = process.env.GB_TOKEN
+config.graciepost.postFile = process.env.GB_POSTFILE
 module.exports = config
