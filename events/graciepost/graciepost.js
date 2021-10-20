4cc29890f31e6ec38daf4e464afa589442ec117d
Gracie Bell <daineesvang@gmail.com>
2021-10-20 22:28:25 +0000

    graciepost now triggers the like button directly

--- events/graciepost/graciepost.js
+++ events/graciepost/graciepost.js
@@ -22,8 +22,12 @@ module.exports = class GraciePost {
     if (meta.overrideEmbed) {
       channel.send(meta.postLink).then(this.likeButton.addButton)
     } else {
-      if (meta.desc && meta.desc.length > 180) {
-        meta.desc = meta.desc.substring(0, 180) + "..."
+      if (meta.desc) {
+        if (meta.desc.includes("\n\n")) { // cut off the description at a double line break
+          meta.desc = meta.desc.substring(0, meta.desc.indexOf("\n\n")) + " [ . . . ]"
+        } else if (meta.desc.length > 180) { // cut off the description if it's too long
+           meta.desc = meta.desc.substring(0, 180) + " [ . . . ]"
+        }
       }
       let embed = {
         title:  meta.title || "",
@@ -45,7 +49,7 @@ module.exports = class GraciePost {
           text: "Retrieved from " + meta.siteName + " using GraciePost"
         }
       }
-
+      console.log(embed);
       channel.send({embed}).then(this.likeButton.addButton)
     }
   }
